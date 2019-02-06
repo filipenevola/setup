3c4587125ff10c60d869480b1d2bc605df2cfe07
filipenevola <filipemkv@gmail.com>
2019-02-06 09:06:07 -0300

    adding db scripts

--- .oh-my-zsh/custom/my-pathable.zsh
+++ .oh-my-zsh/custom/my-pathable.zsh
@@ -1,1 +1,4 @@
 alias apply-hotfix="cd /Users/filipenevola/Documents/pathable/ws/deploy/pathable-next/config/jenkins/ && ./apply-hotfix.sh"
+alias copy-production="cd /Users/filipenevola/Documents/pathable/ws/pathable-next/config/db/backups/production && ./copydb.sh"
+alias restore-production="cd /Users/filipenevola/Documents/pathable/ws/pathable-next/config/db/backups/production && ./restoredb.sh"
+alias copy-restore-production="copy-production && restore-production"
