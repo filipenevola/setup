b4d75929d35d7de8046a6d851fd3a9dd8ea29740
filipenevola <filipemkv@gmail.com>
2019-02-07 13:20:57 -0300

    deploy production bemarke

--- .oh-my-zsh/custom/quave.zsh
+++ .oh-my-zsh/custom/quave.zsh
@@ -1,1 +1,2 @@
 alias bemarke-deploy-staging="cd /Users/filipenevola/Documents/quave/ws/deploys/staging/bemarke/private/env/staging && ./prepare-deploy.sh"
+alias bemarke-deploy-production="cd /Users/filipenevola/Documents/quave/ws/deploys/production/bemarke/private/env/production && ./prepare-deploy.sh"
