9c88dd7c80a848360ff5f58f83858e45cfaff4af
filipenevola <filipemkv@gmail.com>
2019-02-07 09:41:16 -0300

    release development

--- .oh-my-zsh/custom/my-pathable.zsh
+++ .oh-my-zsh/custom/my-pathable.zsh
@@ -1,4 +1,5 @@
 alias apply-hotfix="cd /Users/filipenevola/Documents/pathable/ws/deploy/pathable-next/config/jenkins/ && ./apply-hotfix.sh"
+alias release-development="cd /Users/filipenevola/Documents/pathable/ws/deploy/pathable-next/config/jenkins/ && ./prepare-production-deploy.sh"
 alias copy-production="cd /Users/filipenevola/Documents/pathable/ws/pathable-next/config/db/backups/production && ./copydb.sh"
 alias restore-production="cd /Users/filipenevola/Documents/pathable/ws/pathable-next/config/db/backups/production && ./restoredb.sh"
 alias copy-restore-production="copy-production && restore-production"
