70b2c2771de8ad209299019cbbd89c2a9f20675f
filipenevola <filipemkv@gmail.com>
2019-02-11 11:08:42 -0300

    adding build commands

--- .oh-my-zsh/custom/quave.zsh
+++ .oh-my-zsh/custom/quave.zsh
@@ -1,2 +1,4 @@
 alias bemarke-deploy-staging="cd /Users/filipenevola/Documents/quave/ws/deploys/staging/bemarke/private/env/staging && ./prepare-deploy.sh"
 alias bemarke-deploy-production="cd /Users/filipenevola/Documents/quave/ws/deploys/production/bemarke/private/env/production && ./prepare-deploy.sh"
+alias bemarke-build-staging="cd /Users/filipenevola/Documents/quave/ws/deploys/staging/bemarke && git reset --hard && git checkout staging && git pull && cd private/native-app && ./build-staging.sh"
+alias bemarke-build-production="cd /Users/filipenevola/Documents/quave/ws/deploys/production/bemarke && git reset --hard && git checkout master && git pull && cd private/native-app && ./build-production.sh"
