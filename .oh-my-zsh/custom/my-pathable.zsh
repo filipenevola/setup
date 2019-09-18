42887aa1110c161d595e7a524a585c5c939b79dc
Filipe Névola <filipemkv@gmail.com>
2019-09-18 09:09:52 +0200

    Update my-pathable.zsh

--- .oh-my-zsh/custom/my-pathable.zsh
+++ .oh-my-zsh/custom/my-pathable.zsh
@@ -1,4 +1,5 @@
 alias pathable-apply-hotfix="cd /Users/filipenevola/Documents/pathable/ws/deploy/pathable-next/config/jenkins/ && ./apply-hotfix.sh"
+alias pathable-prepare-branch-from-development="cd /Users/filipenevola/Documents/pathable/ws/deploy/pathable-next/config/jenkins/ && ./prepare-branch-from-development.sh"
 alias pathable-prepare-staging-from-development="cd /Users/filipenevola/Documents/pathable/ws/deploy/pathable-next/config/jenkins/ && ./prepare-staging-deploy.sh"
 alias pathable-prepare-production-from-development="cd /Users/filipenevola/Documents/pathable/ws/deploy/pathable-next/config/jenkins/ && ./prepare-production-deploy.sh"
 alias pathable-prepare-release-patch="cd /Users/filipenevola/Documents/pathable/ws/deploy/pathable-next/config/jenkins/ && ./prepare-release-patch.sh"
