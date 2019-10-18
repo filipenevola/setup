6af48cd38dab1e7dfe7b99894c6b9f5accad5951
filipenevola <filipemkv@gmail.com>
2019-10-18 08:19:07 +0200

    alias bemarke-copyrestore-to-staging="cd /Users/filipenevola/Documents/quave/ws/deploys/production/bemarke/private/env/production && ./copy-restore-db-to-staging.sh"

--- .oh-my-zsh/custom/quave.zsh
+++ .oh-my-zsh/custom/quave.zsh
@@ -7,11 +7,8 @@ alias bemarke-deploy-production="cd /Users/filipenevola/Documents/quave/ws/deplo
 alias bemarke-prepare-and-deploy-production="bemarke-prepare-production && bemarke-deploy-production"
 alias bemarke-build-staging="cd /Users/filipenevola/Documents/quave/ws/deploys/staging/bemarke && git reset --hard && git checkout staging && git pull && cd private/native-app && ./build-staging.sh"
 alias bemarke-build-production="cd /Users/filipenevola/Documents/quave/ws/deploys && git reset --hard && git checkout master && git pull && cd private/native-app && ./build-production.sh"
-alias bemarke-copy-production="cd /Users/filipenevola/Documents/quave/ws/deploys/production/bemarke/private/env/production && ./copydb.sh"
-alias bemarke-restore-production="cd /Users/filipenevola/Documents/quave/ws/deploys/production/bemarke/private/env/production && ./restoredb.sh"
-alias bemarke-copy-restore-production="bemarke-copy-production && bemarke-restore-production"
-alias bemarke-restore-production-to-staging="cd /Users/filipenevola/Documents/quave/ws/deploys/production/bemarke/private/env/production && ./restoredb-to-staging.sh"
 alias bemarke-apply-hotfix="cd /Users/filipenevola/Documents/quave/ws/deploys/production/bemarke/private/config/jenkins && ./apply-hotfix.sh"
 alias ps-meteor="ps aux|grep 'meteor\|max-old-space-size=\|json --port'"
-alias bemarke-copyrestoredb="cd /Users/filipenevola/Documents/quave/ws/deploys/production/bemarke/private/env/production && ./copy-fast-restore-27020-db.sh"
+alias bemarke-copyrestore="cd /Users/filipenevola/Documents/quave/ws/deploys/production/bemarke/private/env/production && ./copy-fast-restore-27020-db.sh"
+alias bemarke-copyrestore-to-staging="cd /Users/filipenevola/Documents/quave/ws/deploys/production/bemarke/private/env/production && ./copy-restore-db-to-staging.sh"
 
